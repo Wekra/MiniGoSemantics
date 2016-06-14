614c8e7291d461ff36bc5bb92281f4b535ebc496
Christian Wernet <wech1020@hs-karlsruhe.de>
2016-06-14 21:40:22 +0200

    Clean-up .gitignore and remove unused imports.

--- src/main/java/de/wernet/minigosemantics/antlr/MiniGoLexer.java
+++ src/main/java/de/wernet/minigosemantics/antlr/MiniGoLexer.java
@@ -1,14 +1,9 @@
 // Generated from E:/Studium/IDEA/MiniGoSemantics/src/main/antlr4/de/wernet/miniGoSemantics/antlr\MiniGo.g4 by ANTLR 4.5.3
 package de.wernet.minigosemantics.antlr;
 
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MiniGoLexer extends Lexer {
