2e3e6b022a63034a6a2b732885d703ad20bc3d3a
Christian Wernet <wech1020@hs-karlsruhe.de>
2016-06-20 21:41:43 +0200

    Implement a sample custom visitor.

--- src/main/java/de/wernet/minigosemantics/Main.java
+++ src/main/java/de/wernet/minigosemantics/Main.java
@@ -1,19 +1,54 @@
 package de.wernet.minigosemantics;
 
+import de.wernet.minigosemantics.antlr.MiniGoLexer;
+import de.wernet.minigosemantics.antlr.MiniGoParser;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
 /**
  * Created by Christian on 13.06.2016.
  */
 public class Main {
 
     public static void main(String []args){
-        System.out.println("Start parsing the file:");
+//        System.out.println("Start parsing the file:");
+//
+//        MyMiniGoListener listener = new MyMiniGoListener();
+//
+        System.out.println(args[0]);
+//
+//        listener.printFile(args[0]);
+//
+//        System.out.println("Parsing successfully finished.");
 
-        MyMiniGoListener listener = new MyMiniGoListener();
+            // TODO code application logic here
+            //SymbolTable table = new SymbolTable();
+            try {
+                ANTLRFileStream reader = new ANTLRFileStream("E:\\Studium\\IDEA\\MiniGoSemantics\\src\\main\\resources\\Beispiel.txt");
+                MiniGoLexer lexer  = new MiniGoLexer((CharStream)reader);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                MiniGoParser parser = new MiniGoParser(tokens);
+                ParseTree tree = parser.prog();
 
-//        System.out.println(args[0]);
-
-        listener.printFile(args[0]);
-
-        System.out.println("Parsing successfully finished.");
+                //ParseTreeWalker walker = new ParseTreeWalker(); // create standard walker
+                //ExtractMicroBaseListener extractor = new ExtractMicroBaseListener(parser);
+                //walker.walk(extractor, tree); // initiate walk of tree with listener
+                MyMiniGoVisitor visitor = new MyMiniGoVisitor();
+                int t = (int) visitor.visit(tree);
+                //for(String str : extractor.table.checkDuplicate()){
+                //    System.out.println("SHADOW WARNING " + str);
+                //}
+                //System.out.println(extractor.table.checkDuplicate().toString());
+                //System.out.println(extractor.table.toString());
+                //System.out.println("Accepted");
+            }catch (IOException e) {
+                System.out.println("Not Accepted");
+            }catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
-}
--- src/main/java/de/wernet/minigosemantics/MyMiniGoVisitor.java
+++ src/main/java/de/wernet/minigosemantics/MyMiniGoVisitor.java
@@ -1,10 +1,101 @@
 package de.wernet.minigosemantics;
 
+import com.sun.istack.internal.NotNull;
 import de.wernet.minigosemantics.antlr.MiniGoBaseVisitor;
+import de.wernet.minigosemantics.antlr.MiniGoParser;
+import de.wernet.minigosemantics.antlr.MiniGoVisitor;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 /**
  * Created by Christian on 14.06.2016.
  */
-public class MyMiniGoVisitor extends MiniGoBaseVisitor {
+public class MyMiniGoVisitor extends MiniGoBaseVisitor implements MiniGoVisitor {
 
+
+    @Override
+    public Object visitProg(@NotNull MiniGoParser.ProgContext ctx) {
+        super.visitProg(ctx);
+        System.out.println("Prog: " + ctx.getText());
+        return 1;
+    }
+
+    @Override
+    public Object visitBlock(MiniGoParser.BlockContext ctx) {
+        super.visitBlock(ctx);
+        System.out.println("Block: " + ctx.getText());
+        return 2;
+    }
+
+    @Override
+    public Object visitStatement(MiniGoParser.StatementContext ctx) {
+        super.visitStatement(ctx);
+        System.out.println("Statement: " + ctx.getText());
+        return 3;
+    }
+
+    @Override
+    public Object visitBexp(MiniGoParser.BexpContext ctx) {
+        super.visitBexp(ctx);
+        System.out.println("Bexp: " + ctx.getText());
+        return 4;
+    }
+
+    @Override
+    public Object visitCexp(MiniGoParser.CexpContext ctx) {
+        super.visitCexp(ctx);
+        System.out.println("Cexp: " + ctx.getText());
+        return 5;
+    }
+
+    @Override
+    public Object visitCterm(MiniGoParser.CtermContext ctx) {
+        super.visitCterm(ctx);
+        System.out.println("Cterm: " + ctx.getText());
+        return 6;
+    }
+
+    @Override
+    public Object visitAexp(MiniGoParser.AexpContext ctx) {
+        super.visitAexp(ctx);
+        System.out.println("Aexp: " + ctx.getText());
+        return 7;
+    }
+
+    @Override
+    public Object visitTerm(MiniGoParser.TermContext ctx) {
+        super.visitTerm(ctx);
+        System.out.println("Term: " + ctx.getText());
+        return 8;
+    }
+
+    @Override
+    public Object visitFactor(MiniGoParser.FactorContext ctx) {
+        super.visitFactor(ctx);
+        System.out.println("Factor: " + ctx.getText());
+        return 9;
+    }
+
+    @Override
+    public Object visitInts(MiniGoParser.IntsContext ctx) {
+        super.visitInts(ctx);
+        System.out.println("INT: " + ctx.getText());
+        return 10;
+    }
+
+    @Override
+    public Object visitBools(MiniGoParser.BoolsContext ctx) {
+        super.visitBools(ctx);
+        System.out.println("BOOL: " + ctx.getText());
+        return 11;
+    }
+
+    @Override
+    public Object visitVars(MiniGoParser.VarsContext ctx) {
+        super.visitVars(ctx);
+        System.out.println("Vars: " + ctx.getText());
+        return 12;
+    }
 }
